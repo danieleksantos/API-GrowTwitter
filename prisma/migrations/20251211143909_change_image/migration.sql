f80b091d27377b05bb98436ec26918660c1cb9ab
Daniele Karina dos Santos <danimistron@gmail.com>
2025-12-11 11:42:56 -0300

    update schema

---
+++ prisma/migrations/20251211143909_change_image/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "imageUrl" SET DEFAULT 'https://images.unsplash.com/vector-1739893035028-9e2c61e4cca4?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
